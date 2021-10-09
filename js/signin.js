c4dbfcc4763afb9c6e33e94e40d7970866221d98
TomAnKing <71315446+TomAnKing@users.noreply.github.com>
2021-10-09 16:23:04 +0200

    finished

--- js/signin.js
+++ js/signin.js
@@ -1,46 +1,32 @@
-/* import displayMessage from "./components/displayMessage.js";
+import displayMessage from "./components/displayMessage.js";
 import { saveToken, saveUser } from "./utils/storage.js";
-import { baseUrl } from "./settings/api.js"; */
+import { baseUrl } from "./settings/api.js";
 
 const form = document.querySelector("#loginForm");
 const userName = document.querySelector("#email");
 const userNameError = document.querySelector("#emailError");
 const password = document.querySelector("#password");
 const passwordError = document.querySelector("#passwordError");
+const message = document.querySelector(".message-container");
 
 form.addEventListener("submit", validateForm);
 
 function validateForm(event) {
   event.preventDefault();
 
-  if (validateEmail(email.value)) {
-    userNameError.style.display = "none";
-  } else {
-    userNameError.style.display = "block";
+  message.innerHTML = "";
+
+  const usernameValue = userName.value.trim();
+  const passwordValue = password.value.trim();
+
+  if (usernameValue.length === 0 || passwordValue.length === 0) {
+    return displayMessage("Invalid values", ".message-container");
   }
 
-  if (checkLength(password.value, 0) === true) {
-    passwordError.style.display = "none";
-  } else {
-    passwordError.style.display = "block";
-  }
+  completeLogin(usernameValue, passwordValue);
 }
 
-function checkLength(value, len) {
-  if (value.trim().length > len) {
-    return true;
-  } else {
-    return false;
-  }
-}
-
-function validateEmail(email) {
-  const regEx = /\S+@\S+\.\S+/;
-  const patternMatches = regEx.test(email);
-  return patternMatches;
-}
-
-/* async function completeLogin() {
+async function completeLogin(username, password) {
   const url = baseUrl + "auth/local";
 
   const data = JSON.stringify({ identifier: username, password: password });
@@ -56,7 +42,19 @@ function validateEmail(email) {
   try {
     const response = await fetch(url, options);
     const json = await response.json();
+    console.log(json);
+
+    if (json.user) {
+      window.location = "signedIn.html";
+
+      saveToken(json.jwt);
+      saveUser(json.user);
+    }
+
+    if (!json.jwt) {
+      displayMessage("Wrong username or password", ".message-container");
+    }
   } catch (error) {
-    console.log(error);
+    console.log("error");
   }
-} */
+}
